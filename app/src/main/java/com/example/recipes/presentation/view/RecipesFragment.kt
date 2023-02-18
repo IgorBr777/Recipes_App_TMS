5c525c1abdcdbf7368675cba949f967955fbbed1
IgorBr777 <igorbraz14@gmail.com>
2023-02-18 20:21:05 +0300

    added viewBinding, implement fun interface RecipesListener

--- app/src/main/java/com/example/recipes/presentation/view/RecipesFragment.kt
+++ app/src/main/java/com/example/recipes/presentation/view/RecipesFragment.kt
@@ -1,9 +1,41 @@
 package com.example.recipes.presentation.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.recipes.databinding.FragmentRecipesBinding
+import com.example.recipes.presentation.view.adapter.listener.RecipesListener
 
 
-class RecipesFragment : Fragment() {
+class RecipesFragment : Fragment(), RecipesListener {
+
+    private var _viewBinding: FragmentRecipesBinding? = null
+    private val viewBinding get() = _viewBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _viewBinding = FragmentRecipesBinding.inflate(inflater)
+        return viewBinding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
 
 
     }
+
+    override fun onClick() {
+
+    }
+
+    override fun onElementSelected(title: String, image: String) {
+
+    }
+
+}
