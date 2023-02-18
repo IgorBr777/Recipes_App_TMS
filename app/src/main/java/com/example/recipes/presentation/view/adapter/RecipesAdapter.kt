9ced55b6bb19b36294fe6aa63ff53909deba754c
IgorBr777 <igorbraz14@gmail.com>
2023-02-18 20:16:40 +0300

    added RecyclerView (RecipesAdapter, RecipesListener, RecipesViewHolder)

--- app/src/main/java/com/example/recipes/presentation/view/adapter/RecipesAdapter.kt
+++ app/src/main/java/com/example/recipes/presentation/view/adapter/RecipesAdapter.kt
@@ -1,4 +1,42 @@
 package com.example.recipes.presentation.view.adapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recipes.databinding.ItemRecipeBinding
+import com.example.recipes.model.RecipesModel
+import com.example.recipes.presentation.view.adapter.listener.RecipesListener
 
-class RecipesAdapter {
+class RecipesAdapter(
+    private val recipesListener: RecipesListener
+) : RecyclerView.Adapter<RecipesViewHolder>() {
+
+    private var listRecipes = mutableListOf<RecipesModel>()
+
+    fun submitList(list: List<RecipesModel>) {
+        this.listRecipes.clear()
+        this.listRecipes = list.toMutableList()
+        this.notifyDataSetChanged()
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipesViewHolder {
+        val viewBinding = ItemRecipeBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return RecipesViewHolder(viewBinding, recipesListener)
+
+    }
+
+    override fun onBindViewHolder(holder: RecipesViewHolder, position: Int) {
+        holder.bind(listRecipes[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listRecipes.size
+    }
+
+
 }
--- app/src/main/java/com/example/recipes/presentation/view/adapter/listener/RecipesListener.kt
+++ app/src/main/java/com/example/recipes/presentation/view/adapter/listener/RecipesListener.kt
@@ -1,4 +1,8 @@
 package com.example.recipes.presentation.view.adapter.listener
 
 interface RecipesListener {
+    fun onClick()
+
+    fun onElementSelected(title: String, image: String)
+
 }
