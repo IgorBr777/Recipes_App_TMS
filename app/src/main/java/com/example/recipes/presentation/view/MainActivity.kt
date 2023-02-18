3557c415d2403d96404e1b59ea2b1a13df08bb66
IgorBr777 <igorbraz14@gmail.com>
2023-02-18 20:17:32 +0300

    added supportFragmentManager

--- app/src/main/java/com/example/recipes/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/recipes/presentation/view/MainActivity.kt
@@ -8,5 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.activity_container, RecipesFragment())
+        fragmentTransaction.commit()
+
     }
+
 }
