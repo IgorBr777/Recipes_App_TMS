8bcd21df32dccef9fa6f2f37dfd855dbecf934c3
IgorBr777 <igorbraz14@gmail.com>
2023-02-19 20:39:06 +0300

    added fun getRecipes

--- app/src/main/java/com/example/recipes/domain/RecipesInteractor.kt
+++ app/src/main/java/com/example/recipes/domain/RecipesInteractor.kt
@@ -1,4 +1,14 @@
 package com.example.recipes.domain
 
-class RecipesInteractor {
+import com.example.recipes.model.RecipesModel
+import javax.inject.Inject
+
+class RecipesInteractor @Inject constructor
+    (private val recipesRepository: RecipesRepository) {
+
+    suspend fun getRecipes():List<RecipesModel>{
+
+        return recipesRepository.getRecipes()
+    }
+
 }
--- app/src/main/java/com/example/recipes/domain/RecipesRepository.kt
+++ app/src/main/java/com/example/recipes/domain/RecipesRepository.kt
@@ -1,4 +1,9 @@
 package com.example.recipes.domain
 
+import com.example.recipes.model.RecipesModel
+
 interface RecipesRepository {
+
+    suspend fun getRecipes():List<RecipesModel>
+
 }
