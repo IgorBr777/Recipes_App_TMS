24a42eea6ca20d530f4c78eaffe3293c0238bd49
IgorBr777 <igorbraz14@gmail.com>
2023-02-28 20:18:44 +0300

    added Binding, Hilt on MainActivity

--- app/src/main/java/com/example/recipes/presentation/view/MainViewModel.kt
+++ app/src/main/java/com/example/recipes/presentation/view/MainViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.recipes.presentation.view
 
+import androidx.lifecycle.ViewModel
+import com.example.recipes.domain.RecipesInteractor
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel {
+class MainViewModel @Inject constructor(
+    private val recipesInteractor: RecipesInteractor
+):ViewModel() {
 }
