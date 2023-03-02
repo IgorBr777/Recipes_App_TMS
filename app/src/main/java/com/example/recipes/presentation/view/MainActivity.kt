4c46aff2bbbad3c854faecdd0dafff3b9d68e5f5
IgorBr777 <igorbraz14@gmail.com>
2023-03-02 10:04:03 +0300

    added Navigation, Bottom Navigation

--- app/src/main/java/com/example/recipes/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/recipes/presentation/view/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.recipes.presentation.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.activity.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.recipes.R
 import com.example.recipes.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,16 +15,23 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
 
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.add(R.id.activity_container, RecipesFragment())
-        fragmentTransaction.commit()
+        navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.fragmentContainerView
+        ) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        binding.bottomNavigation.setupWithNavController(navController)
 
     }
 
--- app/src/main/java/com/example/recipes/presentation/view/adapter/RecipesAdapter.kt
+++ app/src/main/java/com/example/recipes/presentation/view/adapter/RecipesAdapter.kt
@@ -1,4 +1,5 @@
 package com.example.recipes.presentation.view.adapter
+
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
