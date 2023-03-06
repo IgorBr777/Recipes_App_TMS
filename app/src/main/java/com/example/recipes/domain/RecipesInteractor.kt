d6ec5da02af4b21820281fbbbd08a3831aaadc98
IgorBr777 <igorbraz14@gmail.com>
2023-03-06 14:12:43 +0300

    added methods getFavoriteRecipes,onFavClicked,deleteRecipeFavoriteByTitle

--- app/src/main/java/com/example/recipes/domain/RecipesInteractor.kt
+++ app/src/main/java/com/example/recipes/domain/RecipesInteractor.kt
@@ -1,5 +1,6 @@
 package com.example.recipes.domain
 
+import com.example.recipes.model.FavoriteRecipesModel
 import com.example.recipes.model.RecipesModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -13,15 +14,31 @@ class RecipesInteractor @Inject constructor
     }
 
 
-    suspend fun showRecipes():Flow <List<RecipesModel>> {
+    suspend fun showRecipes(): Flow<List<RecipesModel>> {
 
         return recipesRepository.showRecipes()
     }
 
-    suspend fun findRecipe(searchQuery: String):Flow <List<RecipesModel>> {
-        return recipesRepository.findRecipeEntityByTitle(searchQuery)
+    suspend fun findRecipe(searchQuery: String): Flow<List<RecipesModel>> {
+        return recipesRepository.findRecipeByTitle(searchQuery)
 
     }
 
+    suspend fun getFavoriteRecipes():Flow <List<FavoriteRecipesModel>> {
+
+        return recipesRepository.getFavoriteRecipes()
+
+    }
+
+
+    suspend fun onFavClicked(title: String, isFavorite:Boolean) {
+        val foundRecipe = recipesRepository.findRecipeByTitle(title)
+        recipesRepository.favClicked(foundRecipe, isFavorite)
+    }
+
+
+    suspend fun deleteRecipeFavoriteByTitle(title: String){
+        recipesRepository.deleteRecipeFavoriteByTitle(title)
+    }
 
 }
